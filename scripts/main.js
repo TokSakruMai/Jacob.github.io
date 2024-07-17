deab98fba34671bac95957e40eda337ba69745bd
Takeshi <1173073380@qq.com>
2024-07-17 15:29:01 +0800

    jump among pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,3 @@
-function loadContent(page) {
-  fetch(page)
-      .then(response => {
-          if (!response.ok) {
-              throw new Error('Network response was not ok: ' + response.statusText);
-          }
-          return response.text();
-      })
-      .then(data => {
-          const contentDiv = document.getElementById('content');
-          if (contentDiv) {
-              contentDiv.innerHTML = data;
-          } else {
-              console.error('Element with ID "content" not found.');
-          }
-      })
-      .catch(error => console.error('Error loading content:', error));
+function jumpto(page) {
+  window.location.href = page;
 }
