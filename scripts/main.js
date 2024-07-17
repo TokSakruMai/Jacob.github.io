6dc9810e1cee3a6baff047258b02bb82ad0a3768
Takeshi <1173073380@qq.com>
2024-07-17 14:14:29 +0800

    building

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,18 @@
-let myImage = document.querySelector("img");
-
-myImage.onclick = function () {
-  let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/Fortress.jpg") {
-    myImage.setAttribute("src", "images/Taka.jpg");
-  } else {
-    myImage.setAttribute("src", "images/Fortress.jpg");
-  }
-};
+function loadContent(page) {
+  fetch(page)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok: ' + response.statusText);
+          }
+          return response.text();
+      })
+      .then(data => {
+          const contentDiv = document.getElementById('content');
+          if (contentDiv) {
+              contentDiv.innerHTML = data;
+          } else {
+              console.error('Element with ID "content" not found.');
+          }
+      })
+      .catch(error => console.error('Error loading content:', error));
+}
